422728061d40c95db63e0b6b8dfb9c7e395c62f5
henryjonesinbaraj <henryjonesinbaraj@example.com>
2023-02-14 17:04:32 -0500

    third commit

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
-import fitz
+import re
+my_listy= ["on@3", "two#", "thre'%e"]
+print([re.sub('[^a-zA-Z0-9]+', '', g) for g in my_listy])
 
-reader = fitz.open('myresume.pdf')
-for page in reader:
-    text=page.get_text()
-    
-print(text)
+my_list = ["o:<>n@3", "tw{o'#", "thre%e"]
+removetable = str.maketrans("", "", "@#%'%$#@%^&*({)}:"":><?")
+my_list=[s.translate(removetable) for s in my_list]
+print(my_list)
+words_pdf_list=['appl%%$^$#e', "that's", '50%']
+remove_characters= str.maketrans("", "", "@#%'%$#@%^&*({)}:"":><?")
+words_pdf_list=[s.translate(remove_characters) for s in words_pdf_list]
+print(words_pdf_list)
