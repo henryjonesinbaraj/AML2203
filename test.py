1514ad5d998b7c3243ee189163e78a537e181f9f
henryjonesinbaraj <henryjonesinbaraj@example.com>
2023-02-15 02:22:45 -0500

    next version - streamlit app

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-import re
-my_listy= ["on@3", "two#", "thre'%e"]
-print([re.sub('[^a-zA-Z0-9]+', '', g) for g in my_listy])
-
-my_list = ["o:<>n@3", "tw{o'#", "thre%e"]
-removetable = str.maketrans("", "", "@#%'%$#@%^&*({)}:"":><?")
-my_list=[s.translate(removetable) for s in my_list]
-print(my_list)
-words_pdf_list=['appl%%$^$#e', "that's", '50%']
-remove_characters= str.maketrans("", "", "@#%'%$#@%^&*({)}:"":><?")
-words_pdf_list=[s.translate(remove_characters) for s in words_pdf_list]
-print(words_pdf_list)
+def sum(a,b):
+    c=a+b
+    return c
